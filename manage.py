1f439c40761378e85be7bcb41f18072bc5271c6d
IKERRIZ <okothfaith94@gmail.com>
2019-10-22 12:24:27 +0300

    Create a command to run unittests in the application

--- manage.py
+++ manage.py
@@ -4,13 +4,21 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 
-app = create_app('default')
+app = create_app('development')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
 
 manager.add_command('server', Server)
 manager.add_command('db',MigrateCommand)
+
+manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.shell
 def make_shell_context():
