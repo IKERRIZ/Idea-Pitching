883f563c7eb0512b02a7d148084520ddb2d0f493
IKERRIZ <okothfaith94@gmail.com>
2019-10-22 22:46:43 +0300

    Update three classes category of pitches

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,6 +8,13 @@ class PitchForm(FlaskForm):
 	description = TextAreaField("What would you like to pitch ?",validators=[Required()])
 	category = RadioField('Label', choices=[ ('promotionpitch','promotionpitch'), ('interviewpitch','interviewpitch'),('pickuplines','pickuplines'),('productpitch','productpitch')],validators=[Required()])
 	submit = SubmitField('Submit')
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
+class CommentForm(FlaskForm):
+	description = TextAreaField('Add comment',validators=[Required()])
+	submit = SubmitField()
+
+class UpvoteForm(FlaskForm):
+	submit = SubmitField()
+
+
+class Downvote(FlaskForm):
+	submit = SubmitField()
