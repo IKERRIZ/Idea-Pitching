134d09b3db61d3f5b9ce15f84a786e130eed7891
IKERRIZ <okothfaith94@gmail.com>
2019-10-23 08:21:44 +0300

    Connected the application to send emails through google account

--- config.py
+++ config.py
@@ -1,11 +1,27 @@
 import os
 
 class Config:
+    SECRET_KEY = 'youarestrong'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:7995@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
-    pass
+    '''
+    Pruduction configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 
 class DevConfig(Config):
     DEBUG = True
