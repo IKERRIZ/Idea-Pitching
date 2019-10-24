d207b2e734d43f727596abc3450ab9305de8ecc2
IKERRIZ <okothfaith94@gmail.com>
2019-10-24 11:57:19 +0300

    Add SQALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = 'youarestrong'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:7995@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-
+    
+    
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -14,19 +15,36 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    DEBUG = True
+
 class ProdConfig(Config):
     '''
     Pruduction configuration child class
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
 
+class TestConfig(Config):
+   '''
+   Testing configuration child class
+  
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
+
+   pass
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
+    
+
     DEBUG = True
 
 config_options ={
     'development':DevConfig,
-    'production':ProdConfig
+    'production':ProdConfig,
+    'test': TestConfig
     }
