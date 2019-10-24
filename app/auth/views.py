602bc3d7ab62b1634b86ad4ca8b75549a03874fc
IKERRIZ <okothfaith94@gmail.com>
2019-10-24 12:09:00 +0300

    Add a register function to allow new user to regist in the application

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
-from flask import render_template,redirect, url_for
+from flask import render_template,redirect,flash,url_for
 from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
+from ..email import mail_message
 
 
 @auth.route('/login',methods=['GET','POST'])
@@ -13,7 +14,8 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect('/')
+            return render_template('login.html')
 
         flash('Invalid username or Password')
 
@@ -25,3 +27,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to one-minute-pitch","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
