126f8d1789df53923d07a3f5cb5a4e0fad28e677
IKERRIZ <okothfaith94@gmail.com>
2019-10-24 12:06:09 +0300

    Import flask_mail module

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,14 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-
+mail = Mail()
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 simple = SimpleMDE()
@@ -19,6 +20,7 @@ def create_app(config_name):
     app = Flask(__name__)
 
     simple.init_app(app)
+    mail.init_app(app)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
