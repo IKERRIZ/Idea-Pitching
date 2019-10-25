94ade79f887ca4ee59ed4639e8fc01dede532e99
IKERRIZ <okothfaith94@gmail.com>
2019-10-25 08:24:13 +0300

    Placed my database URI in class production

--- config.py
+++ config.py
@@ -19,29 +19,14 @@ class Config:
     DEBUG = True
 
 class ProdConfig(Config):
-    '''
-    Pruduction configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-class TestConfig(Config):
-
-    '''
-    Testing configuration child class
-  
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
 
-    DEBUG = True
-
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 class DevConfig(Config):
-   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+    DEBUG = True
    
 
 config_options ={
