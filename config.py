53c4f5a96c9cd8298dc73c51165740345ccac98b
IKERRIZ <okothfaith94@gmail.com>
2019-10-25 09:05:21 +0300

    Fix database configuration

--- config.py
+++ config.py
@@ -2,13 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-    
-    
-
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
@@ -19,18 +16,16 @@ class Config:
     DEBUG = True
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
-
-class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+    
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:faith@localhost/pitch'
 
     DEBUG = True
    
 
 config_options ={
     'development':DevConfig,
-    'production':ProdConfig,
-    'test': TestConfig
+    'production':ProdConfig
     }
